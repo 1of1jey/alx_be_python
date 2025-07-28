177f69ab9275d0438199fb83e357d2f751b19d52
jeffrey <eshunjeffrey12@gmail.com>
2025-07-28 15:55:43 +0000

    update

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,3 +1,8 @@
 class BankAccount:
     def __init__(self, initial_balance=0.0):
         self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
