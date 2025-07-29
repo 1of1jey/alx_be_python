171cc000955df86373eee2d5a088a51cdcfad818
jeffrey <eshunjeffrey12@gmail.com>
2025-07-29 14:11:00 +0000

    update

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,14 @@
+class Book:
+    """Represents a single book with title, author, and checkout status."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Marks the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
