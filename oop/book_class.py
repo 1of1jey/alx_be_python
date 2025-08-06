1cfaee22c99bf34ce50cf66144e2af4d0e81b959
jeffrey <eshunjeffrey12@gmail.com>
2025-08-06 14:14:16 +0000

    update

--- oop/book_class.py
+++ oop/book_class.py
@@ -8,3 +8,7 @@ class Book:
     def __del__(self):
         """Destructor that prints a message when the book object is deleted."""
         print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """Return informal string representation of the book."""
+        return f"{self.title} by {self.author}, published in {self.year}"
