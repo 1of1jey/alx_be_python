fde13cae5888f0177233f7adc3477238e7cc8b13
jeffrey <eshunjeffrey12@gmail.com>
2025-08-06 14:12:37 +0000

    update

---
+++ oop/book_class.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, author, year):
+        """Initialize a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
