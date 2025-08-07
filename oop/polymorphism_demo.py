2f08290301746dcf886f40dbb9f3145f2218a282
jeffrey <eshunjeffrey12@gmail.com>
2025-08-07 15:36:04 +0000

    update

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -9,11 +9,5 @@ class Shape:
         """Rectangle class that inherits from Shape."""
 
         def __init__(self, length, width):
-            """Initialize a Rectangle with length and width.
-
-            Args:
-                length (float): The length of the rectangle.
-                width (float): The width of the rectangle.
-            """
             self.length = length
             self.width = width
