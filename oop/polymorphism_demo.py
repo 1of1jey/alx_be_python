08628f013a619ea1d3830005dde454881ff96cb7
jeffrey <eshunjeffrey12@gmail.com>
2025-08-07 15:36:55 +0000

    update

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -6,8 +6,6 @@ class Shape:
         raise NotImplementedError("Subclasses must override the area() method")
 
     class Rectangle(Shape):
-        """Rectangle class that inherits from Shape."""
-
         def __init__(self, length, width):
             self.length = length
             self.width = width
